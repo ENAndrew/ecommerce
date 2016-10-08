90ffd63b05346caadc65c7c0e4140b6347301442
Erica Andrew <erica.nicole.andrew@gmail.com>
2016-10-08 14:45:48 -0600

    Moved API into public directory, updated index.js with appropriate locations.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-
+i
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var mongoose = require('mongoose');
-var mainController = require('../api/controllers/mainController');
+var mainController = require('../public/api/controllers/mainController');
 var nodemailer = require('nodemailer');
 
 
@@ -28,31 +28,31 @@ mongoose.connection.once('open', function(){
 
 
 //add product
-app.post('/api/products', mainController.addProduct);
+app.post('/public/api/products', mainController.addProduct);
 
 //add comment to product
-app.put('/api/products/comments/:id', mainController.addComment); //embedding, not using schema
+app.put('/public/api/products/comments/:id', mainController.addComment); //embedding, not using schema
 
 //find all products
-app.get('api/products', mainController.getProducts);
+app.get('/public/api/products', mainController.getProducts);
 
 //find product by query
-app.get('/api/products', mainController.queryProduct);
+app.get('/public/api/products', mainController.queryProduct);
 
 //find product by id
-app.get('/api/products/:id', mainController.findProdById);
+app.get('/public/api/products/:id', mainController.findProdById);
 
 //change product by ID / req.body contents
-app.put('/api/products/:id', mainController.updateProdById);
+app.put('/public/api/products/:id', mainController.updateProdById);
 
 //delete product by ID
-app.delete('/api/products/:id', mainController.deleteProdById);
+app.delete('/public/api/products/:id', mainController.deleteProdById);
 
 //delete comment by ID
-app.put('/api/products/:productId/:commentId', mainController.deleteCommById);
+app.put('/public/api/products/:productId/:commentId', mainController.deleteCommById);
 
 //send Email 
-app.post('/api/postEmail', function(req, res){
+app.post('/public/api/postEmail', function(req, res){
         
         //Transporter object for nodemailer
         var transporter = nodemailer.createTransport({
